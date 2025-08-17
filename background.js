91b3a258ec5f754e133e64321f5c5136e0607342
zhwatts <zach@zachwatts.online>
2025-08-17 14:01:26 -0400

    changes chrome toolbar icon

--- background.js
+++ background.js
@@ -7,4 +7,16 @@ chrome.action.onClicked.addListener(async (tab) => {
   chrome.tabs.sendMessage(tab.id, {
     action: toggled ? "enableCSS" : "disableCSS",
   });
+
+  // Update the icon
+  chrome.action.setIcon({
+    tabId: tab.id,
+    path: toggled
+      ? {
+          128: "icons/maximizer-icon-on.png",
+        }
+      : {
+          128: "icons/maximizer-icon-off.png",
+        },
+  });
 });
